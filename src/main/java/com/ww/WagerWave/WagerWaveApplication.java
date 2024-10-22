fb5b84a0b27cabee17e2d6edb9728b020f5edf85
adamzulu123 <120639376+adamzulu123@users.noreply.github.com>
2024-10-22 13:07:37 +0200

    initial

---
+++ src/main/java/com/ww/WagerWave/WagerWaveApplication.java
@@ -0,0 +1,13 @@
+package com.ww.WagerWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagerWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagerWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ww/WagerWave/WagerWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ww.WagerWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagerWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
