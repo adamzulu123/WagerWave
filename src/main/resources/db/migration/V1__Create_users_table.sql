7072bbcdb4d086f58fd5e1d43b8e792422e5daf4
adamzulu123 <120639376+adamzulu123@users.noreply.github.com>
2024-10-26 01:46:38 +0200

    1

--- src/main/resources/db/migration/V1__Create_users_table.sql
+++ src/main/resources/db/migration/V1__Create_users_table.sql
@@ -1,11 +1,13 @@
-CREATE TABLE Users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       first_name VARCHAR(255) NOT NULL,
-                       last_name VARCHAR(255) NOT NULL,
-                       password VARCHAR(255) NOT NULL,
-                       email VARCHAR(255) NOT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                       age INT NOT NULL,
-                       gender ENUM('M', 'F') NOT NULL,
-                       verified_status TINYINT DEFAULT 0
+CREATE TABLE Users
+(
+    id              INT AUTO_INCREMENT      NOT NULL,
+    first_name      VARCHAR(255)            NOT NULL,
+    last_name       VARCHAR(255)            NOT NULL,
+    password        VARCHAR(255)            NOT NULL,
+    email           VARCHAR(255)            NOT NULL,
+    created_at      TIMESTAMP DEFAULT NOW() NULL,
+    age             INT                     NOT NULL,
+    gender          VARCHAR(1) NOT NULL,  -- Typ już poprawny
+    verified_status BIT DEFAULT 0 NULL,   -- Zmiana z TINYINT na BIT
+    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
 );
